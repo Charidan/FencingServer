5d80654c2b0294ce52f7b313450f179ee0f00b9d
Mel Nicholson <mhnicholson@yahoo.com>
2011-06-06 14:41:10 -0700

    Fixed tabs to whitespace.  Showed Richard preferences to prevent recurrance.

--- src/mel/fencing/server/Server.java
+++ src/mel/fencing/server/Server.java
@@ -7,25 +7,25 @@ import java.util.ArrayList;
 public class Server
 {
     static ArrayList<UserSession> sessions = new ArrayList<UserSession>();
-    
+
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        //TODO publish the ServerSocket and accept connections
-	try
-	{
-	    ServerSocket server = new ServerSocket(0);
-	    while(true)
-	    {
-		UserSession s = UserSession.makeSession(server.accept());
-		if(s != null) sessions.add(s);
-	    }
-	} catch (IOException e)
-	{
-	    System.err.println(e.getMessage());
-	}
+        try
+        {
+            ServerSocket server = new ServerSocket(0);
+            while (true)
+            {
+                UserSession s = UserSession.makeSession(server.accept());
+                if (s != null) sessions.add(s);
+            }
+        }
+        catch (IOException e)
+        {
+            System.err.println(e.getMessage());
+        }
     }
 
 }
--- src/mel/fencing/server/UserSession.java
+++ src/mel/fencing/server/UserSession.java
@@ -10,23 +10,24 @@ public class UserSession extends Thread
     private Socket s;
     private BufferedInputStream in;
     private PrintStream out;
-    
+
     private UserSession(Socket socket) throws IOException
     {
-	s = socket;
-	in = new BufferedInputStream(s.getInputStream());
-	out = new PrintStream(s.getOutputStream());
+        s = socket;
+        in = new BufferedInputStream(s.getInputStream());
+        out = new PrintStream(s.getOutputStream());
     }
-    
+
     public static UserSession makeSession(Socket socket)
     {
-	try
-	{
-	    return new UserSession(socket);
-	} catch (IOException e)
-	{
-	    System.err.println(e.getMessage());
-	    return null;
-	}
+        try
+        {
+            return new UserSession(socket);
+        }
+        catch (IOException e)
+        {
+            System.err.println(e.getMessage());
+            return null;
+        }
     }
 }
