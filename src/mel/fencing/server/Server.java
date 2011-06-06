9adad667a38ea2abd36f56f1d6cd7a227dab5fa0
Mel Nicholson <mhnicholson@yahoo.com>
2011-06-06 12:39:15 -0700

    Test.

--- src/mel/fencing/server/Server.java
+++ src/mel/fencing/server/Server.java
@@ -8,8 +8,8 @@ public class Server
      */
     public static void main(String[] args)
     {
-	//TODO publish the ServerSocket and accept connections
-
+        //TODO publish the ServerSocket and accept connections
+        //Test Comment - To be Removed
     }
 
 }
